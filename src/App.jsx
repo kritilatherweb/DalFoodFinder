d0d10800772210513604bf01735b34126491774f
kritilatherweb <klather2003@gmail.com>
2025-10-28 19:09:03 +0530

    ba

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import Header from './components/Header'
-
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='w-full overflow-hidden'>
     <Header/>
-
-     
-    </>
+    <About/>
+  
+    </div>
   )
 }
 
