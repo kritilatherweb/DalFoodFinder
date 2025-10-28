62a03f940d62c424df8e9987c578ef09afd483f0
kritilatherweb <klather2003@gmail.com>
2025-10-28 18:06:01 +0530

    renew

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Navbar from './components/Navbar'
 import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
     <Header/>
+
      
     </>
   )
